9e6e27ccbec700e9196faea76fc539a5a8f8f1b4
easipes2021 <easipes2021@gmail.com>
2026-03-28 13:54:43 -0500

    Update page.js

--- woka-next/app/page.js
+++ woka-next/app/page.js
@@ -1,10 +1,30 @@
 "use client";
 
+import AirTempCard from "./components/AirTempCard";
+
 export default function Page() {
   return (
-    <main>
-      <h1>WOKA Next.js Preview</h1>
-      <p>If you see this, the build worked.</p>
+    <main style={styles.container}>
+      <h1 style={styles.header}>WOKA Dashboard Preview</h1>
+
+      {/* Vertical stacking layout */}
+      <AirTempCard />
+
+      {/* More cards will be added below */}
     </main>
   );
 }
+
+const styles = {
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: "20px"
+  },
+  header: {
+    fontSize: "28px",
+    fontWeight: "700",
+    marginBottom: "24px"
+  }
+};
