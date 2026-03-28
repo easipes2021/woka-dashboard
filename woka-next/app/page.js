d2418ce22dbcb02910681572b6cb0af65c78501e
easipes2021 <easipes2021@gmail.com>
2026-03-28 13:42:02 -0500

    Create page.js

---
+++ woka-next/app/page.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Page() {
+  return (
+    <main>
+      <h1>WOKA Next.js Preview</h1>
+      <p>If you see this, the build worked.</p>
+    </main>
+  );
+}
