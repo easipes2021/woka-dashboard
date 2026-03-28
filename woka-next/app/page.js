b5248e400ad071e757f500f3a166d88384004b68
easipes2021 <easipes2021@gmail.com>
2026-03-28 09:17:56 -0500

    Update page.js

--- woka-next/app/page.js
+++ woka-next/app/page.js
@@ -1,1 +1,21 @@
+"use client";
 
+import { useEffect } from "react";
+
+export default function Home() {
+  useEffect(() => {
+    console.log("Dashboard mounted in Next.js");
+  }, []);
+
+  return (
+    <main style={{ padding: "20px" }}>
+      <h1>WOKA Dashboard (Next.js Version)</h1>
+
+      <p>
+        The dashboard components will appear here as we migrate them piece by piece.
+      </p>
+
+      <div id="dashboard-root"></div>
+    </main>
+  );
+}
