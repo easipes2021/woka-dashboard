c452ee5501cba8effbc260c7ba94f69be654abc8
easipes2021 <easipes2021@gmail.com>
2026-03-28 13:41:44 -0500

    Create layout.js

---
+++ woka-next/app/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "WOKA Next Preview"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  );
+}
